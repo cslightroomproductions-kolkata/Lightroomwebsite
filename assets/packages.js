77da02b67debfe36fd168ea15142d618cc4edca6
cslightroomproductions-kolkata <cs.lightroomproductions@gmail.com>
2026-09-21 20:37:00 +0530

    Update packages.js

--- assets/packages.js
+++ assets/packages.js
@@ -67,3 +67,4 @@ const PACKAGE_GROUPS = {
     {name:"Festival Elite",price:74999,days:"Full Production",events:["Festival / large event"],team:"Full production + Drone + Gimbal",deliverables:["Stage + crowd coverage","15-min film","6 reels","Express highlight","USB"]}
   ]
 };
+module.exports = PACKAGE_GROUPS;
